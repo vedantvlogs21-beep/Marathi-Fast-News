69f876a8c0c5d21fbc1287059d485fa17f091ce7
vedantvlogs21-beep <vedantvlogs21@gmail.com>
2026-06-07 19:37:54 +0530

    Include XML stylesheet in sitemap output
    Add XML stylesheet reference to sitemap generation.

--- server.ts
+++ server.ts
@@ -853,6 +853,7 @@ app.get("/sitemap.xml", async (req: any, res: any) => {
     }
     
      let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+    xml += `<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>\n`;
     xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
     
     // Add homepage
